2d2e098cc02d98c36adc5a5f573e103bbdfd2f14
LGDEMO <1292575030@qq.com>
2019-11-23 21:14:53 +0800

    拦截实现

--- src/main/java/com/endata/springboot/intercepors/LoginInterceptor.java
+++ src/main/java/com/endata/springboot/intercepors/LoginInterceptor.java
@@ -6,6 +6,7 @@ import com.endata.springboot.util.JWTUtil;
 import com.sun.istack.internal.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,32 +29,37 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     //这个方法是在访问接口之前执行的，我们只需要在这里写验证登陆状态的业务逻辑，就可以在用户调用指定接口之前验证登陆状态了
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //每一个项目对于登陆的实现逻辑都有所区别，我这里使用最简单的Session提取User来验证登陆。
-        HttpSession session = request.getSession();
-        //这里的User是登陆时放入session的
-        response.setContentType("text/html;charset=UTF-8");
-        // 告诉服务器 请求的内容是什么编码格式
-        request.setCharacterEncoding("UTF-8");
-        Cookie[] cookies = request.getCookies();
-        String  userName = null;
-        if (cookies !=null) {
-            for (Cookie cookie : cookies) {
-                String token = cookie.getValue();
-                String s  =  JWTUtil.getUsername(token);
-                if(s!=null){
-                    userName  = s;
+
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            return true;
+        } else {
+            //每一个项目对于登陆的实现逻辑都有所区别，我这里使用最简单的Session提取User来验证登陆。
+            HttpSession session = request.getSession();
+            //这里的User是登陆时放入session的
+            response.setContentType("text/html;charset=UTF-8");
+            // 告诉服务器 请求的内容是什么编码格式
+            request.setCharacterEncoding("UTF-8");
+            Cookie[] cookies = request.getCookies();
+            String userName = null;
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    String token = cookie.getValue();
+                    String s = JWTUtil.getUsername(token);
+                    if (s != null) {
+                        userName = s;
+                    }
                 }
             }
-        }
-        User user = userService.getDataByUserName(userName);
+            User user = userService.getDataByUserName(userName);
 
-        //如果session中没有user，表示没登陆
-        if (user == null){
-            //这个方法返回false表示忽略当前请求，如果一个用户调用了需要登陆才能使用的接口，如果他没有登陆这里会直接忽略掉
-            //当然你可以利用response给用户返回一些提示信息，告诉他没登陆
-            return false;
-        }else {
-            return true;    //如果session里有user，表示该用户已经登陆，放行，用户即可继续调用自己需要的接口
+            //如果session中没有user，表示没登陆
+            if (user == null) {
+                //这个方法返回false表示忽略当前请求，如果一个用户调用了需要登陆才能使用的接口，如果他没有登陆这里会直接忽略掉
+                //当然你可以利用response给用户返回一些提示信息，告诉他没登陆
+                return false;
+            } else {
+                return true;    //如果session里有user，表示该用户已经登陆，放行，用户即可继续调用自己需要的接口
+            }
         }
     }
 
